111e0ceff2db06d395d794927002978d1a5d0fac
AmmanSajid1 <ammansajid1@gmail.com>
2025-02-19 16:56:13 +0000

    Project structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name="AI Cyber Security Assistant",
+    version='0.0.0',
+    author="Amman Sajid",
+    author_email="amman33@hotmail.com", 
+    packages=find_packages(), 
+    install_requires=[],
+)
